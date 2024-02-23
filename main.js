9845be5766805ac9d8607ee2c8195e8318178ef7
Asraful Islam Masrafi <asrafulislammasrafi@gmail.com>
2024-02-23 22:34:47 +0600

    add few section

--- main.js
+++ main.js
@@ -2,9 +2,20 @@ const navOpen = document.querySelector(".open");
 const navClose = document.querySelector(".close");
 const icon = document.querySelector(".icon");
 const nav = document.querySelector("nav");
+const play = document.querySelector(".play");
 
 icon.addEventListener("click", function () {
   navOpen.classList.toggle("dn");
   navClose.classList.toggle("dn");
   nav.classList.toggle("activeNav");
+});
+
+const video1 = document.querySelector("#video1");
+play.addEventListener("click", function () {
+  play.style.display = "none";
+  video1.play()
+});
+video1.addEventListener("click", function () {
+  play.style.display = "block";
+  video1.pause()
 });
